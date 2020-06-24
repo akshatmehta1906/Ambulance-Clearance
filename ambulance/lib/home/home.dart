d228d665915877694a933c3e689de399f0c21264
Akshat_Mehta_ <akshatmehta1906@gmail.com>
2020-06-24 14:31:29 +0530

    uj
    uj

--- ambulance/lib/home/home.dart
+++ ambulance/lib/home/home.dart
@@ -109,9 +109,8 @@ class _HomeState extends State<Home> {
   void updateLocation() async {
     print("updating ...");
 
-    alat=ambLat();
-    along=ambLong();
-    finaldist= distanceInBetween( _position.latitude.toDouble(), _position.longitude.toDouble());
+
+
 
 
     try {
@@ -122,8 +121,9 @@ class _HomeState extends State<Home> {
       setState(() {
         _position = newPosition;
       });
+      finaldist= distanceInBetween( _position.latitude.toDouble(), _position.longitude.toDouble());
 
-      Firestore.instance.collection("ID").document("789IsAs3uthnB0Re1oZS9jpQ0Gz2").updateData({
+      Firestore.instance.collection("ID").document("6WJEVTcYWWPn6wY4SwsfaW7UGcv2").updateData({
         'longitude': _position.longitude.toDouble(),
         'latitude': _position.latitude.toDouble(),
         'distance': finaldist,
