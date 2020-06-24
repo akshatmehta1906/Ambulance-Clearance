37005750ee673fc7aa5051c35ee858fd725b6e3f
hardik_here <1027hardik@gmail.com>
2020-06-24 15:18:45 +0530

    back to home

--- ambulance/lib/home/home.dart
+++ ambulance/lib/home/home.dart
@@ -10,38 +10,6 @@ import 'package:ambulance/map/flutter_maps.dart';
 import 'package:ambulance/shared/constants.dart';
 import 'package:ambulance/shared/loading.dart';
 import 'package:ambulance/models/user.dart';
-import 'package:ambulance/alarm.dart';
-
-final db= Firestore.instance.collection('ID');
-final ambDB = Firestore.instance.collection('ID').document("V4BFp3NYtXhP6WO4DEdOckmD6fH3");
-
-
-dynamic ambLat() async {
-
-  final documents = await db.where('name', isEqualTo: "ambulance").getDocuments();
-  var ambLat = documents.documents.first.data['latitude'];
-
-  return (ambLat);
-}
-
-dynamic ambLong() async {
-
-  final documents = await db.where('name', isEqualTo: "ambulance").getDocuments();
-  var ambLong = documents.documents.first.data['longitude'];
-
-  return ambLong;
-}
-
-
-dynamic distanceInBetween (double lat2, double long2) async {
-
-  double distanceInMeters;
-
-  distanceInMeters = await Geolocator().distanceBetween(ambLat(),ambLong(),lat2 , long2);
-  return distanceInMeters;
-}
-
-
 
 
 
@@ -53,6 +21,71 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('RastaDo'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        actions: <Widget>[
+          FlatButton.icon(
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              icon: Icon(Icons.person),
+              label: Text('Logout')),
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Go to maps',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => flutterMap()));
+                },
+                child: Text('Location'),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                'Get Location',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => GeolocationExample()));
+                },
+                child: Text('Coordinates'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GeolocationExampleState extends State {
   final _formKey = GlobalKey<FormState>();
   Geolocator _geolocator;
   Position _position;
@@ -60,11 +93,6 @@ class _HomeState extends State<Home> {
   double _long;
   String _name = "no name";
   Timer _timer;
-  double alat;
-  double along;
-  double finaldist;
-
-
 
   void checkPermission() {
     _geolocator.checkGeolocationPermissionStatus().then((status) {
@@ -108,12 +136,7 @@ class _HomeState extends State<Home> {
 
   void updateLocation() async {
     print("updating ...");
-
-
-
-
     try {
-
       Position newPosition = await Geolocator()
           .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
           .timeout(new Duration(seconds: 5));
@@ -121,12 +144,14 @@ class _HomeState extends State<Home> {
       setState(() {
         _position = newPosition;
       });
-      finaldist= distanceInBetween( _position.latitude.toDouble(), _position.longitude.toDouble());
-
-      Firestore.instance.collection("ID").document("6WJEVTcYWWPn6wY4SwsfaW7UGcv2").updateData({
+//      await DatabaseService(uid: "XbyZRbkk4jUfgppZzCacALcgAiH3").updateUserData(
+//        'asdfgh ' ?? userData.name,
+//        _lat ?? userData.latitude,
+//        _long ?? userData.longitude,
+//      );
+      Firestore.instance.collection("ID").document("789IsAs3uthnB0Re1oZS9jpQ0Gz2").updateData({
         'longitude': _position.longitude.toDouble(),
         'latitude': _position.latitude.toDouble(),
-        'distance': finaldist,
       });
     } catch (e) {
       print('Error: ${e.toString()}');
@@ -137,8 +162,8 @@ class _HomeState extends State<Home> {
 
     final user = Provider.of<User>(context);
     DatabaseService(uid: user.uid).userData;
-    key:_formKey;
-
+    key:
+    _formKey;
     //UserData userData=snapshot.data;
 
     // DatabaseService(uid: user.uid).updateUserData(
@@ -151,66 +176,61 @@ class _HomeState extends State<Home> {
         builder: (context, snapshot) {
           UserData userData = snapshot.data;
           DatabaseService(uid: user.uid).updateUserData(' ' ?? userData.name,
-              _lat ?? userData.latitude, _long ?? userData.longitude, finaldist ?? userData.distance, 0?? userData.speed);
+              _lat ?? userData.latitude, _long ?? userData.longitude, 0 ?? userData.speed, 0 ?? userData.distance);
         });
-
-
   }
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('RastaDo'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Logout')),
-        ],
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
-          child: Column(
-            children: <Widget>[
-              Text(
-                'Alarm',
-                style: TextStyle(color: Colors.white),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              RaisedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Alarm()));
-                },
-                child: Text('Location'),
-              ),
-              Column(
+    final user = Provider.of<User>(context);
+
+
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          UserData userData = snapshot.data;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Coordinates'),
+            ),
+            key: _formKey,
+            body: SafeArea(
+              child: Column(
                 children: <Widget>[
-                  Text(
-                    'Latitude: ${_position != null ? _position.latitude.toString() : '0'},'
-                        ' Longitude: ${_position != null ? _position.longitude.toString() : '0'},'
-                        'Distance: ${_position != null ? finaldist : '0'}'
-                    ,
+                  Text('Latitude: '
+                      '${_position != null ? _position.latitude.toString() : '0'},'
+                      ' Longitude: ${_position != null ? _position.longitude.toString() : '0'}',),
+
+                  RaisedButton(
+                    color: Colors.pink,
+                    child: Text(
+                      'Back',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {
+//                      {
+//                        await DatabaseService(uid: user.uid).updateUserData(
+//                          'asdfgh ' ?? userData.name,
+//                          _lat ?? userData.latitude,
+//                          _long ?? userData.longitude,
+//                        );
+                      Navigator.pop(context);
+//                      }
+                    },
                   ),
-
-
                 ],
               ),
-
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
+        });
   }
 }
+
+class GeolocationExample extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return GeolocationExampleState();
+  }
+}
+
+
+
